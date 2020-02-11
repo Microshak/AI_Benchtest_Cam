3c9c1904f43f3ac5bea88ea0954c76de68c94da5
mike roshak <roshak@hotmail.com>
2020-02-11 14:28:30 -0800

    camera stream

--- app.py
+++ app.py
@@ -10,6 +10,9 @@ import json
 import requests
 from flask_apscheduler import APScheduler
 import socket
+from PIL import Image
+from io import BytesIO
+import numpy as np
 
 app = Flask(__name__)
 scheduler = APScheduler()
@@ -42,8 +45,53 @@ def manifest():
     print(txt)
 
 
+
+
+
+
+
+
+
+
+
+def gen( height,width, downsample):
+ 
+
+    while True:
+      url = f'http://localhost:5000/image.jpg?height={height}&width={width}'
+      r = requests.get(url) # replace with your ip address
+      curr_img = Image.open(BytesIO(r.content))
+      
+      frame = cv2.cvtColor(np.array(curr_img), cv2.COLOR_RGB2BGR)
+      dwidth = float(width) * (1 - float(downsample))
+      dheight = float(height) * (1 - float(downsample))
+      frame = imutils.resize(frame, width=int(dwidth), height=int(dheight))
+
+      
+      frame = cv2.imencode('.jpg', frame)[1].tobytes()      
+      yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+
+@app.route('/stream.jpg')
+def stream():
+    
+    height = request.args.get('height')
+    width = request.args.get('width')
+    downsample = request.args.get('downsample')
+ 
+    """Returns a single current image for the webcam"""
+    return Response(gen(height,width, downsample), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+
+
+
+
 if __name__ == '__main__':
     manifest()
     scheduler.add_job(id ='Scheduled task', func = manifest, trigger = 'interval', minutes = 10)
     scheduler.start()    
     app.run(host='0.0.0.0', threaded=True)
+
+    
